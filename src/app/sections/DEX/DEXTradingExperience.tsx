3ef0bf6ffb56525822b0213ed3a40831f83f4d06
Amit Nirala <amit@Amits-MacBook-Pro.local>
2024-04-09 16:44:58 +0530

    Images changed for mobile view and Chain website scroll bug fix

--- src/app/sections/DEX/DEXTradingExperience.tsx
+++ src/app/sections/DEX/DEXTradingExperience.tsx
@@ -7,16 +7,43 @@ const DEXTradingExperience: React.FC = () => {
   return (
     <section className={styles.tradingExperience}>
       <h1 className="heading_primary">
-      Unbeatable Trading Tools for a hassle-free Trading Experience
+        Unbeatable Trading Tools for a hassle-free Trading Experience
       </h1>
       {/* <h2 className="heading_primary_sub">Advanced Layer 3 Derivatives DEX, operating as a Validium AppChain on Variable’s Layer 2 scaling solution.</h2> */}
-      <div className={styles.tradingExperience_image1}>
-      <Image src={"/UnbeateableTradingImage.svg"} alt="Unbeateable Trading Image" height={100} width={1200}/>
+      <div className={styles.tradingExperience_desktop}>
+        <div className={styles.tradingExperience_image1}>
+          <Image
+            src={"/UnbeateableTradingImage.svg"}
+            alt="Unbeateable Trading Image"
+            height={100}
+            width={1200}
+          />
+        </div>
+        <div className="my-16 "></div>
+        <Image
+          src={"/InteractiveChartTrading.svg"}
+          alt="Variable mockup"
+          height={100}
+          width={1200}
+        />
       </div>
-    <div className="my-16 "></div>
-      <Image src={"/InteractiveChartTrading.svg"} alt="Variable mockup" height={100} width={1200}/>
+      <div className={styles.tradingExperience_mobile}>
+        <Image
+          src={"/UnbeatableimageMobile1.svg"}
+          alt="Unbeateable Trading Image"
+          height={100}
+          width={1200}
+        />
+        <div className="my-16 "></div>
+        <Image
+          src={"/UnbeatableimageMobile2.svg"}
+          alt="Unbeateable Trading Image"
+          height={100}
+          width={1200}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default DEXTradingExperience;
